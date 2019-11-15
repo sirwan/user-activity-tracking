24b0d9a24776e47eda642dbcf368986c00bc9455
Sirwan Qutbi <sirwanqutbi@gmail.com>
2019-11-15 21:38:19 +0000

    Create user-activity-tracking.php

---
+++ user-activity-tracking.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: User Activity Tracking
+Plugin URI: https://sirwan.me/plugin/wordpress/show-user-last-visit
+Description: Show date and time of a users last visit in the All Users admin page. This field could be exported in a CSV spreadsheet too, from any other plugin.
+Author: Sirwan Qutbi
+Version: 1.0.0
+Author URI: https://sirwan.me/
+*/
+
